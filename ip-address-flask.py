e470a5670edaad346f5e257150983109dccafcaa
Diego Cambiaso <diegocambiaso@gmail.com>
2023-07-11 07:32:09 -0500

    Create ip-address-flask.py

---
+++ ip-address-flask.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    ip_address = request.remote_addr
+    return f"Your IP address is: {ip_address}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
